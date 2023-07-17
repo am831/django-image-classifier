d52c9db934e3a7622828f27407c16ca946c7c27d
Alisha <alisha831maddy@gmail.com>
2023-07-17 06:37:27 -0700

    revert

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Django Medical Image Classifier
-A Django webapp that uses machine learning models to classify images of melanoma and brain MRI scans as benign vs malignant
+A Django webapp that uses machine learning models to classify images of melanoma and brain MRI scans as benign vs malignant. The user can upload images which are stored in an aws s3 bucket.
+
+# Image Classification with Scikit Learn
+Two different machine learning models were trained using datasets downloaded from Kaggle. One model can determine if a brain MRI image contains a tumor or not, the other can determine if a skin abnormality is melanoma or not. Neural networks were built with Scikit-Learn.
+
+# Installation
+
+# Usage
+
+# Built With
+
+- Web Development:
+Django | HTML | CSS
+
+- Python 3.11 / Nueral Network
+Scikit-Learn | Numpy
+
+- Database
+AWS S3
--- webapp/settings.py
+++ webapp/settings.py
@@ -23,10 +23,28 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY="django-insecure-7(mnpmn%s6b&b*q3*#u*v9ustf%=m^ryi5u1yo3^0cx9n56h15"
+SECRET_KEY="django-insecure-7(mnpmn%s6b&b*q3*#u*v9ustf%=m^ryi5u1yo3^0cx9n56h15"
 
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False  
+AWS_DEFAULT_ACL = None  
+AWS_S3_REGION_NAME=os.environ.get('AWS_S3_REGION_NAME')  
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_LOCATION = 'static'
+PUBLIC_MEDIA_LOCATION = 'media'
+WEBSITE_HOSTNAME="med-image-classify.azurewebsites.net/"
+
+
+
+
+
+"""
 env = environ.Env()
 environ.Env.read_env()
-SECRET_KEY = env('SECRET_KEY')
+#SECRET_KEY = env('SECRET_KEY')
 AWS_ACCESS_KEY_ID=env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY=env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME=env('AWS_STORAGE_BUCKET_NAME')
@@ -37,11 +55,13 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_LOCATION = 'static'
 PUBLIC_MEDIA_LOCATION = 'media'
-
+"""
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+#ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 
 # Application definition
@@ -92,14 +112,14 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
